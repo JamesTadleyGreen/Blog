d0a020533ffc8959d3b1177bb3397467b1d8cd35
James Green <jamestadleygreen@outlook.com>
2021-06-17 19:21:47 +0100

    Fixing the scrollbar issues

--- react/blog/src/Projects.js
+++ react/blog/src/Projects.js
@@ -14,25 +14,22 @@ function Projects(props) {
     // Render a Thumbnail component
     <div>
       <h1>Projects</h1>
-      <Grid container spacing={3} justify="space-around">
-        <Grid item xs>
+      <Grid container spacing={0} justify="space-around">
+        <Grid item sm>
           <Thumbnail
             link="/proj"
             image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
             title="Portfolio Website"
             category="Website"
-            height={useWindowSize()[1]}
           />
         </Grid>
 
-        <Grid item sm={5}>
+        <Grid item sm={3}>
           <Thumbnail
             link="/proj1"
             image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
             title="Future of Work"
             category="Economics"
-            width="10"
-            height="10"
           />
         </Grid>
       </Grid>
--- react/blog/src/Thumbnail.js
+++ react/blog/src/Thumbnail.js
@@ -1,58 +1,60 @@
 import React from 'react'; // Import the Component component from React
-import {Link} from 'react-router-dom'; // Import the Link component
 import {ImageBackground, View, Text, StyleSheet} from 'react-native'
+import { makeStyles, withTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import Box from '@material-ui/core/Box';
 import './App.css';
  
-class Thumbnail extends React.Component {
-    state = {
-    }
+export default function Thumbnail(props) {
+    const styles = useStyles();
 
-    static getDerivedStateFromProps(nextProps, prevState) {
-        if (prevState !== nextProps) {
-          return { ...nextProps };
-        }
-    
-        return null;
-    } 
-    
-    render () {
-        let className = "project"
-        className += " thumbnail"
-        console.log(this.state)
-        return (
-        <div className={className} height={this.state.height}>
-            <Link to={this.state.link}>
-            <div className="project-image">
-                <View style={styles.thumbImage}>
-                    <ImageBackground source={this.state.image} alt="Project">
-                        <View style={styles.thumbText}>
-                            <Text>{this.state.title}</Text>
-                            <Text>{this.state.category}</Text>
-                        </View>
-                    </ImageBackground>
-                </View>
-            </div>
-            </Link>
-        </div>
-        );
-    }
+    console.log(props)
+    return (
+        <Card className={styles.card}>
+        <CardMedia
+            className={styles.media}
+            image={
+            props.image
+            }
+        />
+        <Box py={3} px={2} className={styles.content}>
+            <Text className={styles.boxText}>asd</Text>
+        </Box>
+        </Card>
+    );
 }
 
-const styles = StyleSheet.create({
-    thumbImage: {
-        flex: 1,
-        justifyContent: "space-between",
-        backgroundColor: "#fff",
-        padding: 0,
-        margin: 10,
-        borderRadius: 10,
-        overflow: 'hidden'
+const useStyles = makeStyles((theme) => ({
+    card: {
+        borderRadius: '1rem',
+        boxShadow: 'none',
+        position: 'relative',
+        margin: 20,
+        '&:after': {
+          content: '""',
+          display: 'block',
+          position: 'absolute',
+          width: '100%',
+          height: '64%',
+          bottom: 0,
+          zIndex: 1,
+          background: 'linear-gradient(to top, #000, rgba(0,0,0,0))',
+        },
+      },
+    content: {
+        position: 'absolute',
+        zIndex: 2,
+        bottom: 0,
+        width: '100%',
+      },
+    boxText: {
+        color: "#ffffff",
     },
-    thumbText: {
-        color: "#fff",
-        backgroundColor: "#ffffff90",
-
-    },
-});
-
-export default Thumbnail;
+    media: {
+        height: 0,
+        height:"100%",
+        width:"100%",
+        paddingTop: '56.25%', // 16:9
+      },
+    }));
